70a467edc53e1648b5c8075c4349bca3ac855103
Nick Sagona <nick@nolainteractive.com>
2015-06-21 22:59:46 -0500

    Write unit tests

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Pop Version Component
 =====================
 
+[![Build Status](https://travis-ci.org/popphp/pop-version.svg?branch=master)](https://travis-ci.org/popphp/pop-version)
---
+++ tests/VersionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pop\Version\Test;
+
+use Pop\Version\Version;
+
+class VersionTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCompare()
+    {
+        $this->assertGreaterThan(0, Version::compareVersion('1.0'));
+    }
+
+    public function testGetLatest()
+    {
+        $this->assertGreaterThan(1, Version::getLatest());
+    }
+
+    public function testIsLatest()
+    {
+        $this->assertTrue(Version::isLatest());
+    }
+
+    public function testSystemCheck()
+    {
+        $system = Version::systemCheck();
+        $this->assertEquals(PHP_VERSION, $system['php']['installed']);
+    }
+
+}
